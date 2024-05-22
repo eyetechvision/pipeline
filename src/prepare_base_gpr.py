4020019389c4e9a9cc4f3978022cb8b823725ad9
Ian <yetian.su@gmail.com>
2024-05-22 16:10:08 +0800

    gpr base for df_high directly

--- src/prepare_base_gpr.py
+++ src/prepare_base_gpr.py
@@ -1,7 +1,6 @@
-# gpr model on df_high data,
-# to predict AL from other features
-# in order to combine df_low
-
+# gpr model on df_high
+# to predict diopter_s from AL/CR
+# as a baseline
 
 import pandas as pd
 import numpy as np
@@ -19,18 +18,25 @@ def main():
         sys.exit(1)
 
     input_path = sys.argv[1]
-    output_path_train = sys.argv[2] + "/gpr_train_data.pt"
-    output_path_test = sys.argv[2] + "/gpr_test_data.pt"
+    output_path_train = sys.argv[2] + "/base_gpr_train_data.pt"
+    output_path_test = sys.argv[2] + "/base_gpr_test_data.pt"
 
     data = pd.read_csv(input_path, delimiter=",")
 
     ###### Data Preprocessing ######
-    X = data[["age", "diopter_s", "CR", "gender"]]
-    y = data["AL"]
-
+    X = data[["age", "AL", "CR", "gender"]]
+    y = pd.to_numeric(data["diopter_s"], errors="coerce")
     X = X.apply(pd.to_numeric, errors="coerce")
-    X = X.dropna()
-    y = y.loc[X.index]
+    # X = X.dropna()
+    # y = y.loc[X.index]
+    # Replace inf values with nan
+    X.replace([np.inf, -np.inf], np.nan, inplace=True)
+    y.replace([np.inf, -np.inf], np.nan, inplace=True)
+
+    # Drop rows with nan values in X or y
+    valid_data = X.join(y).dropna()
+    X = valid_data[X.columns]
+    y = valid_data[y.name]
 
     X_tensor = torch.tensor(X.values).float()
     y_tensor = torch.tensor(y.values).float()
